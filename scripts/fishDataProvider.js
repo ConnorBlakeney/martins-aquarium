1e5da939150e9ff3fa1a1e3406ad43f233c5ae8f
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-14 10:39:43 -0500

    added fish data provider

---
+++ scripts/fishDataProvider.js
@@ -0,0 +1,25 @@
+const fishCollection = [
+  {
+    name: "Dory",
+    food: "Plankton",
+    species: "Blue Tang",
+    length: "4 inches",
+    location: "Great Barrier Reef",
+  },
+
+  {
+    name: "Nemo",
+    food: "Plankton, Algae",
+    species: "Clownfish",
+    length: "5 inches",
+    location: "Great Barrier Reef",
+  },
+
+  {
+    name: "Bruce",
+    food: "Fish, Tires",
+    species: "Great White Shark",
+    length: "25 feet",
+    location: "Great Barrier Reef",
+  },
+];
